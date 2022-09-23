34b2b89243ef1e9a23561b3205773550101dd8a9
MarianaPenaAntuna <marianapenamovil@gmail.com>
2022-09-23 10:01:00 -0300

    component addTask just only generate

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,26 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule} from '@angular/common/http';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/header/header.component';
+import { ButtonComponent } from './components/button/button.component';
+import { TasksComponent } from './components/tasks/tasks.component';
+import { TasksItemComponent } from './components/tasks-item/tasks-item.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    ButtonComponent,
+    TasksComponent,
+    TasksItemComponent,
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FontAwesomeModule,
+    HttpClientModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/mock-tasks.ts
@@ -0,0 +1,22 @@
+import {Task} from "./Task";
+
+export const TASKS : Task[] = [
+    {
+        id: 1,
+        text: "terminar primer modulo de angular",
+        day: "agosto 5 a las 12.00",
+        reminder: true
+    },
+    {
+        id: 2,
+        text: "terminar de leer panda",
+        day: "agosto 5 a las 12.00",
+        reminder: true
+    },
+    {
+        id: 3,
+        text: "empezar con el portfolio en angular",
+        day: "enero 10 a las 12.00",
+        reminder: true
+    }
+]
